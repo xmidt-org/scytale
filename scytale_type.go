a9753bcfa9db9c4a3d5f10e176b8cd93b074313d
Kristina Spring <kmspring57@gmail.com>
2019-08-30 13:28:21 -0700

    Updated bascule and code to match

--- scytale_type.go
+++ scytale_type.go
@@ -18,16 +18,16 @@
 package main
 
 import (
+	"github.com/xmidt-org/bascule"
 	"github.com/xmidt-org/bascule/key"
-	"github.com/xmidt-org/webpa-common/secure"
 )
 
 //JWTValidator provides a convenient way to define jwt validator through config files
 type JWTValidator struct {
 	// JWTKeys is used to create the key.Resolver for JWT verification keys
-	Keys key.ResolverFactory `json:"keys"`
+	Keys key.ResolverFactory
 
-	// Custom is an optional configuration section that defines
-	// custom rules for validation over and above the standard RFC rules.
-	Custom secure.JWTValidatorFactory `json:"custom"`
+	// Leeway is used to set the amount of time buffer should be given to JWT
+	// time values, such as nbf
+	Leeway bascule.Leeway
 }

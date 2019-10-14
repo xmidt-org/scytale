c8229b8b443d50d0f5fef9e2f28876d9b6f96e75
James C Scott III <jcscottiii@users.noreply.github.com>
2019-10-14 14:07:26 -0400

    Improve Application Versioning (#66)
    * Improve Application Versioning
    
    This will allow for better bug reports and information the binary running.
    This information can be retrieved upon providing the -v or --version
    flag
    
    * fixup: fix update-version and use variables to be more consistent

--- main.go
+++ main.go
@@ -19,9 +19,11 @@ package main
 
 import (
 	"fmt"
+	"io"
 	_ "net/http/pprof"
 	"os"
 	"os/signal"
+	"runtime"
 
 	"github.com/go-kit/kit/log/level"
 	"github.com/spf13/pflag"
@@ -40,9 +42,14 @@ const (
 	//DefaultKeyID is used to build JWT validators
 	DefaultKeyID = "current"
 
-	applicationName    = "scytale"
-	release            = "Developer"
-	applicationVersion = "0.1.5"
+	applicationName = "scytale"
+	release         = "Developer"
+)
+
+var (
+	GitCommit = "undefined"
+	Version   = "undefined"
+	BuildTime = "undefined"
 )
 
 // scytale is the driver function for Scytale.  It performs everything main() would do,
@@ -58,6 +65,18 @@ func scytale(arguments []string) int {
 
 		logger, metricsRegistry, webPA, err = server.Initialize(applicationName, arguments, f, v, webhook.Metrics, aws.Metrics, basculechecks.Metrics)
 	)
+
+	if parseErr, done := printVersion(f, arguments); done {
+		// if we're done, we're exiting no matter what
+		if parseErr != nil {
+			friendlyError := fmt.Sprintf("failed to parse arguments. detailed error: %s", parseErr)
+			logging.Error(logger).Log(
+				logging.ErrorKey(),
+				friendlyError)
+			os.Exit(1)
+		}
+		os.Exit(0)
+	}
 
 	if err != nil {
 		fmt.Fprintf(os.Stderr, "Unable to initialize Viper environment: %s\n", err)
@@ -117,6 +136,28 @@ func scytale(arguments []string) int {
 	close(shutdown)
 	waitGroup.Wait()
 	return 0
+}
+
+func printVersion(f *pflag.FlagSet, arguments []string) (error, bool) {
+	printVer := f.BoolP("version", "v", false, "displays the version number")
+	if err := f.Parse(arguments); err != nil {
+		return err, true
+	}
+
+	if *printVer {
+		printVersionInfo(os.Stdout)
+		return nil, true
+	}
+	return nil, false
+}
+
+func printVersionInfo(writer io.Writer) {
+	fmt.Fprintf(writer, "%s:\n", applicationName)
+	fmt.Fprintf(writer, "  version: \t%s\n", Version)
+	fmt.Fprintf(writer, "  go version: \t%s\n", runtime.Version())
+	fmt.Fprintf(writer, "  built time: \t%s\n", BuildTime)
+	fmt.Fprintf(writer, "  git commit: \t%s\n", GitCommit)
+	fmt.Fprintf(writer, "  os/arch: \t%s/%s\n", runtime.GOOS, runtime.GOARCH)
 }
 
 func main() {

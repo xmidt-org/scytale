bd8e321074e5880f442b8134cc6c590d8562c0f3
Kristina Pathak <kmspring57@gmail.com>
2021-11-22 14:59:19 -0800

    fixed string slice casting issue

--- basculeValidators.go
+++ basculeValidators.go
@@ -5,17 +5,21 @@ import (
 	"errors"
 	"fmt"
 
+	"github.com/spf13/cast"
 	"github.com/xmidt-org/bascule"
 	"github.com/xmidt-org/webpa-common/v2/basculechecks"
 )
-
+ 
 var requirePartnersJWTClaim bascule.ValidatorFunc = func(_ context.Context, token bascule.Token) error {
 	partnerVal, ok := bascule.GetNestedAttribute(token.Attributes(), basculechecks.PartnerKeys()...)
 	if !ok {
 		return fmt.Errorf("Partner IDs not found at keys %v", basculechecks.PartnerKeys())
 	}
-	ids, ok := partnerVal.([]string)
-	if !ok || len(ids) < 1 {
+	ids, err := cast.ToStringSliceE(partnerVal)
+	if err != nil {
+		return fmt.Errorf("failed to cast partner IDs to []string: %v", err)
+	}
+	if len(ids) < 1 {
 		return errors.New("Partner ID JWT claim should be a non-empty list of strings")
 	}
 	return nil
